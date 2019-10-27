529006b24760262c42d1b0644485dca642c35c7f
KurohataY <shihata001@gmail.com>
2019-10-27 20:57:04 +0900

    AppleWatch加速度習得メソッド

--- PBL WatchKit Extension/InterfaceController.swift
+++ PBL WatchKit Extension/InterfaceController.swift
@@ -1,31 +1,141 @@
 //
 //  InterfaceController.swift
-//  PBL WatchKit Extension
+//  PBL2 WatchKit Extension
 //
-//  Created by KurohataY on 2019/10/27.
+//  Created by KurohataY on 2019/10/09.
 //  Copyright © 2019 KurohataY. All rights reserved.
 //
 
 import WatchKit
 import Foundation
+import WatchConnectivity
+import CoreMotion
+
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+
+    @IBOutlet weak var ActionData: WKInterfaceLabel!
+    
+    //CMMotionManager(データの習得のため)、OperationQueue(複数の非同期処理の終了を待ち合わせ)を利用
+    let motionManager = CMMotionManager()
+    let queue = OperationQueue()
+
+    var randNum =  String(UInt32())
+    
+    //以下データ保存用の変数
+    var applicationDict = [String: String]()
+    //ディバイスの向き
+    var attitude_pitch = ""
+    var attitude_roll = ""
+    var attitude_yaw = ""
+    
+    //重力加速度
+    var gravity_x = ""
+    var gravity_y = ""
+    var gravity_z = ""
+    
+    //デバイスの回転速度
+    var rotationRate_x = ""
+    var rotationRate_y = ""
+    var rotationRate_z = ""
+    
+    //ユーザが与えている加速度
+    var userAcceleration_x = ""
+    var userAcceleration_y = ""
+    var userAcceleration_z = ""
 
 
-class InterfaceController: WKInterfaceController {
-
+    //アプリを立ち上げた時の処理
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+        activateSession()
     }
 
+    //iPhnoeとの通信処理
+    func activateSession(){
+        if WCSession.isSupported() {
+            let session: WCSession = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+        sendMessage()
+    }
+
+    //データ習得＋iPhnoeにデータを送る
+    func sendMessage(){
+        //エラー処理
+        if !motionManager.isDeviceMotionAvailable {
+            print("Device Motion is not available.")
+            return
+        }
+
+        if self.randNum == "" {
+            motionManager.stopDeviceMotionUpdates()
+            return
+        }else{
+            
+            motionManager.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+
+                //エラー処理
+                if error != nil {
+                    print("Encountered error: \(error!)")
+                }
+
+                //データ習得
+                if deviceMotion != nil {
+                    self.attitude_pitch = "\(deviceMotion!.attitude.pitch)"
+                    self.attitude_roll = "\(deviceMotion!.attitude.roll)"
+                    self.attitude_yaw = "\(deviceMotion!.attitude.yaw)"
+                    
+                    self.gravity_x = "\(deviceMotion!.gravity.x)"
+                    self.gravity_y = "\(deviceMotion!.gravity.y)"
+                    self.gravity_z = "\(deviceMotion!.gravity.z)"
+                    
+                    self.rotationRate_x = "\(deviceMotion!.rotationRate.x)"
+                    self.rotationRate_y = "\(deviceMotion!.rotationRate.y)"
+                    self.rotationRate_z = "\(deviceMotion!.rotationRate.z)"
+                    
+                    self.userAcceleration_x = "\(deviceMotion!.userAcceleration.x)"
+                    self.userAcceleration_y = "\(deviceMotion!.userAcceleration.y)"
+                    self.userAcceleration_z = "\(deviceMotion!.userAcceleration.z)"
+                }
+
+                //データを送る処理
+                if WCSession.default.isReachable {
+
+                    let date = Date()
+
+                    self.applicationDict = [
+                        "date": String(describing: date),
+                        "attitude_pitch": self.attitude_pitch,
+                        "attitude_roll": self.attitude_roll,
+                        "attitude_yaw": self.attitude_yaw,
+                        "gravity_x": self.gravity_x,
+                        "gravity_y": self.gravity_y,
+                        "gravity_z": self.gravity_z,
+                        "rotationRate_x": self.rotationRate_x,
+                        "rotationRate_y": self.rotationRate_y,
+                        "rotationRate_z": self.rotationRate_z,
+                        "userAcceleration_x": self.userAcceleration_x,
+                        "userAcceleration_y": self.userAcceleration_y,
+                        "userAcceleration_z": self.userAcceleration_z
+                    ]
+                    sleep(UInt32(0.5))
+                    WCSession.default.sendMessage(self.applicationDict, replyHandler: {(reply) -> Void in
+                        print(reply)
+                    }){(error) -> Void in
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
+
+    @available(watchOS 2.2, *)
+    //エラー処理
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidComplete")
+    }
+
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+    }
 }
--- PBL/ViewController.swift
+++ PBL/ViewController.swift
@@ -1,20 +1,128 @@
 //
 //  ViewController.swift
-//  PBL
+//  PBL2
 //
-//  Created by KurohataY on 2019/10/27.
+//  Created by KurohataY on 2019/10/09.
 //  Copyright © 2019 KurohataY. All rights reserved.
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
 
+    //書き出し用
+    var messageData: String = ""
+    //動作状態
+    var actionState: String!
+    //ラベル
+    var labelArray: [String] = ["date","attitude_picth","attitude_roll","attitude_yaw","gravity_x","gravity_y","gravity_z","rotationRate_x","rotationRate_y","rotationRate_z","userAcceleration_x","userAcceleration_y","userAcceleration_z","action\n"]
+    //AppleWatchのCore Motionデータの格納場所
+    var messageArray: [String] = []
+    
+    //ラベル付け
+    @IBAction func changeButton(_ sender: Any) {
+        actionState = "change"
+    }
+    //ラベル付け
+    @IBAction func oneTouchButton(_ sender: Any) {
+        actionState = "onetouch"
+    }
+    //ラベル付け
+    @IBAction func outButton(_ sender: Any) {
+        actionState = "out"
+    }
+    //ラベル付け
+    @IBAction func noneButton(_ sender: Any) {
+        actionState = ""
+    }
+    
+    //書き込み処理(取り出し方：https://codeday.me/jp/qa/20190301/344079.html)
+    @IBAction func fileWrite(_ sender: Any) {
+        
+        if let dir = FileManager.default.urls( for: .documentDirectory, in: .userDomainMask ).first {
+
+            let filePath = dir.appendingPathComponent( "PBLtest.txt" )
+            self.messageData += messageArray.joined(separator: ",")
+
+            do {
+                print("testing")
+                print("filePath: \(filePath)")
+                try messageData.write(to: filePath, atomically: true, encoding: .utf8)
+            } catch {
+                print("error")
+            }
+        }
+    }
+    //ロード時の処理
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        actionState = ""
+        messageData = labelArray.joined(separator: ",")
+        activateSession()
+        
     }
 
+    //AppleWatchとの通信処理
+    func activateSession(){
+        if WCSession.isSupported() {
+            let session: WCSession = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
 
+    //メッセージを受け取りテキストデータとして格納処理
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+
+        //メッセージを受け取り
+        let params = message
+        //メッセージを受け取りテキストデータとして格納
+        messageArray.append(params["date"] as! String)
+        messageArray.append(params["attitude_pitch"] as! String)
+        messageArray.append(params["attitude_roll"] as! String)
+        messageArray.append(params["attitude_yaw"] as! String)
+        messageArray.append(params["gravity_x"] as! String)
+        messageArray.append(params["gravity_y"] as! String)
+        messageArray.append(params["gravity_z"] as! String)
+        messageArray.append(params["rotationRate_x"] as! String)
+        messageArray.append(params["rotationRate_y"] as! String)
+        messageArray.append(params["rotationRate_z"] as! String)
+        messageArray.append(params["userAcceleration_x"] as! String)
+        messageArray.append(params["userAcceleration_y"] as! String)
+        messageArray.append(params["userAcceleration_z"] as! String)
+        
+        //動作状態のラベル
+        if actionState  == "change"{
+            messageArray.append("change\n")
+            print("change")
+        }else if actionState  == "onetouch"{
+            messageArray.append("onetouch\n")
+            print("onetouch")
+        }else if actionState  == "out"{
+            messageArray.append("out\n")
+            print("out")
+        }else{
+            messageArray.append("none\n")
+            print("none")
+        }
+
+        replyHandler(["reply" : "Done"])
+    }
+
+    @available(iOS 9.3, *)
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidComplete")
+    }
+
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print("sessionDidBecomeInactive")
+    }
+
+    func sessionDidDeactivate(_ session: WCSession) {
+        print("sessionDidDeactivate")
+    }
+    
+    
 }
 
